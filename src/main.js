49337d1c0f51ed703f3da9088db46181218f77db
varinea <varinea.lopez@gmail.com>
2019-09-05 11:48:45 -0400

    cambios html-js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,15 @@
 /* Manejo del DOM */
 
+const all = RICKANDMORTY.results; //declaramos una variable para tomas todo de la data RICKANDMORTY// .results de la data tomo 
+                                    //solo los resultados
+for(let i = 0; i < all.length; i++) // aqui tenemos for declaramos la formula dando una variable llamada i que es igual(=) a 0
+                                      // siendo menor (<) que la variable all y length que recorre la longitud y i++(recorre uno por uno) 
+  { console.log(all[i].status);   } 
+
+  const all = RICKANDMORTY.results; //declaramos una variable para tomas todo de la data RICKANDMORTY// .results de la data tomo 
+                                    //solo los resultados
 
 
-const rickandmorty = () => {
-    return 'RICKANDMORTY';
-  };
-  
-  window.rickandmorty = RICKANDMORTY;
+const recoverValues = Object.values(all) // Esta variable recorre los valores y devuelve lo que esta en all*
+//console.log(recoverValues);
 
-
-// const Genero = document.getElementById("genDer");
-// botonUno.addEventListener("click", () => {
-
-//   var textusuario = document.getElementById("ingText").value;
-//   var numusuario = document.getElementById("ingNum").value;
-//   .encode(textusuario, numusuario);
-//   document.getElementById("textoFinal").innerHTML = cipher.encode(textusuario, numusuario);
-
-// });
