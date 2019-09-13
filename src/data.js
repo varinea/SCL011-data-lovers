5349515ad3076665e0afb9a5ca6e1cc68337e037
varinea <varinea.lopez@gmail.com>
2019-09-13 10:06:47 -0300

    cambios Data

--- src/data.js
+++ src/data.js
@@ -1,28 +1,11 @@
-/* Manejo de data */
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
 
-const rymList = window.RICKANDMORTY.rickandmorty;
- window.rymList= rymList;
+const filterlive = (data,condition) => {
+  const resultFilterLive = data.filter(element => {
 
- //Filtrar por Tipo
-//Recibe dos parámetos: 1.La lista de personajes rym y 2. El 'type' escogido
-const filterTypes = (rymList, selectedType) => {
-    //.filter() crea un nuevo array compuesto por los elementos que cumplen
-    //el criterio de búsqueda, en este caso crearía un arreglo de todos los personajes
-    //que cumplan con el 'type' seleccionado.
-    //trabajamos con 'Element' a modo genérico, en este caso el elemento será la variable
-    //donde se encuentra la lista de pokemons utiizada  en el main.js
-    const resultType = rymList.filter(Element => {
-      //retornará todo elemento donde se encuentre dentro de su array de tipo 'type' según
-      //sea el value seleccionado en el selectBox (html estático)
-        return Element.type.includes(selectedType);
-    });
-    //retorna el valor de la función filtrar
-    return resultType;
+   return element.Alive === condition
+  });
 
-  }
+  return resultFilterLive.Alive;
 
-  //hacemos FilterTypes de modo global para llamarla desde main.js
-    window.filterTypes = filterTypes;
+}
--- src/main.js
+++ src/main.js
@@ -1,126 +1,36 @@
-/*let character_RyM = RICKANDMORTY.results; //Con una variable estoy llamando a la data de RICKANDMORTY en result
+/* Manejo del DOM */
 
-const container = document.getElementById('root') //estoy llamando al id root que esta en html
-button_Ch.addEventListener('click', () => {
-const naNana = document.getElementById('charRyM');
+let RyM = RICKANDMORTY.results;
+//Le doy nombre a mi variable, la cuál tiene la función de llamar la data de rickandmorty.js
 
+document.getElementById("showMeTheCharacters").addEventListener("click", () => {
+  filterRyM.character(RyM);
+  //llamo al id "showMeTheCharacters" de HTML y con un addEve...y la funcion el usuario al hacer
+  // CLICK esta llamando a la variable de los personajes de RICKANDMORTY
+})
 
------------------------------------------------------------------------------------------------*/
+//   PREGUNTAR SI VA EN DATA O EN MAIN??? 
+filterRyM = {  //
+  character: (RyM) => {
+    //(character_RyM) conecto con filter para poder 
 
-//Data global de data y aquí creamos la variable 
-//que almacenará a sus personajes respectivamente
-const randm = window.RICKANDMORTY.rickandmorty;
-for(let i=0; i<randm.length;i++){
+    let resultCharacter = [" "]; // Creo la variable para que recorra for.
+    for (let i = 0; i < RyM.length; i++) { //For recorrer       para concatenar ``
+      resultCharacter +=
 
-  ///Div de clase columna, cada tarjeta corresponderá a una columna dentro de la fila
-  //let columnList = document.createElement("div");
-  //columnList.className = "list";
-  //Crearemos una etiqueta <a> que envolverá la tarjeta, para poder llamar la apertura
-  //de un modal con href
-  /*
-  let linkModal = document.createElement("a");
-  linkModal.href = */
+        `       
+          <li class="character_RyM">
+          <img class="imgrym" src="${RyM[i].image}">
+          <div id="charcter_div"> 
+          <p class="textrym1">  ${RyM[i].name}</p>
+          <p class="textrym1">  ${RyM[i].status}</p>
+          <p class="textrym1">  ${RyM[i].species}</p>
+          <p class="textrym1">  ${RyM[i].origin.name}</p>
+          </div>
+          </li>
+          `
+      document.getElementById("root").innerHTML = resultCharacter;
 
-
-  //Le indicamos que cree un elemento div en nuestro HTML que contendrá toda la info
-  let chip = document.createElement("div");
-  //Aquí le diremos que tome la clase "cards" de css
-  chip.className =  "chip";
-  //Le entregaremos un id
-  chip.id = "chip";
-
-let characterName = document.createElement("h4");
-  //Para rellenar el elemento h4, al ser una etiqueta de texto utilizamos textContent 
-  //para asignarle su contenido
-  //En rickandmorty.js "name"=""
-  characterName.textContent = randm[i].name;
-
-//Creamos el elemento img que conteneŕa la imagen de cada pokemon
-let characterPhoto = document.createElement("img");
-//Le decimos que en este elemento HTML su source(src="") será lo que contiene
-//el elemento img:'' dentro de la data, según sea el índice.
-//En rickandmorty.js "img"=""
-characterPhoto.src = randm[i].img;
-
-let characterSpecies = document.createElement("h3");
-//Para rellenar el elemento h3, al ser una etiqueta de texto utilizamos textContent 
-  //para asignarle su contenido
-  //En rickandmorty.js "species"=""
-characterSpecies.textContent = randm[i].species;
-
-let characterStatus = document.createElement("h5");
-//Para rellenar el elemento h5, al ser una etiqueta de texto utilizamos textContent 
-  //para asignarle su contenido
-  //En rickandmorty.js "status"=""
-characterStatus.textContent = randm[i].status;
-
-let fileType = document.createElement("div");
-  fileType.className = "file";
-  fileType.id = "fileType";
-
-
-let listType = document.createElement("div");
-  listType.className = "list";
-  listType.id = "listType";
-  let columnType = document.createElement("div");
-  columnType.className = "column";
-
-  //Tiene un tipo
-  //Ideal sería hacer un if, que si tiene un tipo entonces cree un label y de acuerdo
-  //sea el tipo, será la clase que tomará (para que sean de colores distintos) y si son
-  //dos tipos entones que cree dos label.
-  let rickandmortyType = document.createElement("h1");
-  rickandmortyType.textContent = randm[i].type;
- 
-    
-    columnType.appendChild(ricjandmortyType);
-    fileType.appendChild(columnType);
-    chip.appendChild(characterName);
-    chip.appendChild(characterPhoto);
-    chip.appendChild(pokemonSatatus);
-    chip.appendChild(filterType);
-
-
-    columnList.appendChild(chip);
-
-    //Mostrar tarjeta en el contenedor especificado
-    document.getElementById("root").appendChild(columnList).innerHTML; 
+    };
   }
-
-
-//Le entregamos a la variable selectorType el id del selector del cual obtendremos
-//el value seleccionado por el usuario
-const selectorType = document.getElementById("filterType");
-//Le agregamos un evento tipo "change", esto significa que cada vez que el elemento de 
-//id "filterType" cambie de value, se ejecutará la función showByType
-selectorType.addEventListener("change", showByType);
-
-function showByType()
-{
-  //Le entregamos a la variable selectedType el valor que ha sido seleccionado desde el html
-
-  let selectedType = selectorType.options[selectorType.selectedIndex].value;
-  //Aquí decimos que en filterResult, se almacenará el resultado de la ejecución de la 
-  //función filterTypes. En este momento ya se encuentran todos los pokemons que cumplen
-  //con el criterio de filtrado
-  let filterResult = window.filterTypes(randm, selectedType);
-  //Como la primera acción del DOM dentro de la web es mostrar todos los elementos de 
-  //rickandmorty.js en vista tarjetas dentro de un div, le indicamos a este que se vacíe para
-  //recibir dentro de él los resultados del filtrado
-  document.getElementById("root").innerHTML="";
-}
-
-  //Ahora como tenemos los resultados en una variable, deberemos recorrer uno a uno los pokemons
-  //para mostrarlos, del mismo modo que en el primer for donde mostramos todos los pokemons.
-  //La diferencia es que en vez de referirnos a pokemon.type,.name,.etc nos referiremos a 
-  //filterResult.name filterResult.img filterResult.number filterResult.type
-  for(let i=0; i<filterResult.length; i++){
-    console.log(filterResult);}
-      //Div de clase columna, cada tarjeta corresponderá a una columna dentro de la fila
-      //let columnFile = document.createElement("div");
-      //columnFile.className = "column";
-  //Crearemos una etiqueta <a> que envolverá la tarjeta, para poder llamar la apertura
-  //de un modal con href
-  /*
-  let linkModal = document.createElement("a");
-  linkModal.href = */
+};
