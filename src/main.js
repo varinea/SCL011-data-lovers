29eb82df5b7e0766d9827015337ccd1bddffe2c7
varinea <varinea.lopez@gmail.com>
2019-09-22 00:17:39 -0300

    cambios htmlCssJs

--- src/main.js
+++ src/main.js
@@ -1,73 +1,70 @@
 /* Manejo del DOM */
 
+
+/*___________________________VIDEO_____________________________________*/
+
+
+var RyMVideo = document.getElementById("videoMyR");
+        function playPause() {
+            if (RyMVideo.paused)
+                RyMVideo.play();
+            else
+                RyMVideo.pause();
+        }
+        function stop() {
+            videoMyR.pause();
+            videoMyR.currentTime = 0;
+        }
+        function skip(value) {
+            videoMyR.currentTime += value;
+        };
+
+
+/*___________________________PERSONAJES_____________________________________*/
+
+
 let RyM = RICKANDMORTY.results;
-//Le doy nombre a mi variable, la cuál tiene la función de llamar la data de rickandmorty.js
+//Le doy nombre a mi variable, la cuál tiene la función de llamar la data de rickandmorty.js resultados.
 
-document.getElementById("showMeTheCharacters").addEventListener("click", () => {
-  filterRyM.character(RyM);
-  //llamo al id "showMeTheCharacters" de HTML y con un addEve...y la funcion el usuario al hacer
-  // CLICK esta llamando a la variable de los personajes de RICKANDMORTY
-})
-
-//   PREGUNTAR SI VA EN DATA O EN MAIN??? 
-filterRyM = {  //
-  character: (RyM) => {
+document.getElementById("showMeTheCharacters").addEventListener("click", () => {filterRyM.character(RyM);})
+ //Desde el HTML llamo al boton y le digo que al hacer click comience la función
+  //creo una variable, objeto y llama a la variable de RyM (La cual contiene la info de data)
+filterRyM = {character: (RyM) => {
     //(character_RyM) conecto con filter para poder 
 
-    let resultCharacter = [" "]; // Creo la variable para que recorra for.
+    let resultCharacter; // Creo la variable para que recorra for.
     for (let i = 0; i < RyM.length; i++) { //For recorrer       para concatenar ``
-      resultCharacter +=
 
-        `       
-          <li class="character_RyM">
+    
+      resultCharacter +=
+      `   
+      <div class="textRyM2"> 
+        <h2>CONOCE A TODOS LOS PERSONAJES</h2>
+        <h1>de Rick and Morty</h1>
+        <br><br>
+        </div>
+
+        <div class="liItem">
+        <br><br><br><br><br><br><br><br><br>
           <img class="imgrym" src="${RyM[i].image}">
-          <div id="charcter_div"> 
+          <div class="charData"> 
           <p class="textrym1">  ${RyM[i].name}</p>
           <p class="textrym1">  ${RyM[i].status}</p>
           <p class="textrym1">  ${RyM[i].species}</p>
           <p class="textrym1">  ${RyM[i].origin.name}</p>
+          </div>    
           </div>
-          </li>
           `
+        
       document.getElementById("root").innerHTML = resultCharacter;
 
     };
   }
 };
 
-
-filterRyM = filterRyM; 
-
-//________________________________________________________________________________________________
-
-
-
-
-const containerRoot = document.getElementById("root") //llamo al root de HTML
-const selectstatus = document.getElementById('Alive') //Creo variable y llamo al estado ALIVE de la data de rickandmorty.js
-
-selectStatus.addEventListener("change" , () => {
-
-  let condition = selectstatus.options [selectstatus.selectIndex].text
-
-  window.filterAlive.Alive(root);
-
-})
+filterRyM = filterRyM;
 
 
 
 
 
-//return resultCharacter;
-//.join('');
-//filtrar
-//var filter_RyM = resultCharacter.filter(dos => dos =="RyM[i].name");
-//document.getElementById(showMetheName).addEventListener"click", ()=>;
-//BUSCAR WINDOW
-
-
-/* FILTRAR
-
-const filterhouse = (DATA, condition)
-
-*/
